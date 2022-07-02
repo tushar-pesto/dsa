b8d9536beb879544296b81196ae25460c5543c8c
Tushar Raj <traj@monsterindia.com>
2022-07-02 09:26:32 +0530

    Create queue-from-stacks.js

---
+++ week-6/exercises/queue-from-stacks.js
@@ -0,0 +1,43 @@
+import MyStack from '../concepts/my-stack';
+
+class QueueFromStacks {
+  constructor() {
+    this.inbox = new MyStack();
+    this.outbox = new MyStack();
+  }
+
+  enqueue(item) {
+    this.inbox.push(item);
+  }
+
+  dequeue() {
+    if (this.outbox.size === 0) {
+      if (this.inbox.size === 0) {
+        throw 'Queue Underflow';
+      } else {
+        while (this.inbox.size > 0) {
+          let item = this.inbox.pop();
+          this.outbox.push(item);
+        }
+      }
+    }
+    let frontItem = this.outbox.pop();
+    // console.log('dequeued', frontItem)
+    return frontItem;
+  }
+
+  get size() {
+    return this.inbox.items.length + this.outbox.items.length;
+  }
+
+  get isEmpty() {
+    return this.size === 0;
+  }
+
+  log() {
+    console.log('inbox: ', this.inbox.items);
+    console.log('outbox: ', this.outbox.items);
+  }
+}
+
+export default QueueFromStacks;
