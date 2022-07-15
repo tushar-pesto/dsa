b38ad7a54614de5673984935bcbcdc9e78f4670b
Tushar Raj <traj@monsterindia.com>
2022-07-15 22:32:21 +0530

    qfs

--- week-6/exercises/queue-from-stacks.js
+++ week-6/exercises/queue-from-stacks.js
@@ -1,43 +1,47 @@
 import MyStack from '../concepts/my-stack';
 
 class QueueFromStacks {
-  constructor() {
-    this.inbox = new MyStack();
-    this.outbox = new MyStack();
-  }
-
-  enqueue(item) {
-    this.inbox.push(item);
-  }
-
-  dequeue() {
-    if (this.outbox.size === 0) {
-      if (this.inbox.size === 0) {
-        throw 'Queue Underflow';
-      } else {
-        while (this.inbox.size > 0) {
-          let item = this.inbox.pop();
-          this.outbox.push(item);
-        }
-      }
+    constructor() {
+        this.inbox = new MyStack();
+        this.outbox = new MyStack();
     }
-    let frontItem = this.outbox.pop();
-    // console.log('dequeued', frontItem)
-    return frontItem;
-  }
 
-  get size() {
-    return this.inbox.items.length + this.outbox.items.length;
-  }
+    enqueue(item) {
+        // push to inbox
+        this.inbox.push(item);
+    }
 
-  get isEmpty() {
-    return this.size === 0;
-  }
+    
+    dequeue() {
+        // if outbox is empty, pop entire inbox into outbox
+        if (this.outbox.size === 0) {
+            if (this.inbox.size === 0) {
+                throw 'Queue Underflow';
+            } else {
+                while (this.inbox.size > 0) {
+                    let item = this.inbox.pop();
+                    this.outbox.push(item);
+                }
+            }
+        }
+        // pop from outbox
+        let frontItem = this.outbox.pop();
+        // console.log('dequeued', frontItem)
+        return frontItem;
+    }
 
-  log() {
-    console.log('inbox: ', this.inbox.items);
-    console.log('outbox: ', this.outbox.items);
-  }
+    get size() {
+        return this.inbox.items.length + this.outbox.items.length;
+    }
+
+    get isEmpty() {
+        return this.size === 0;
+    }
+
+    log() {
+        console.log('inbox: ', this.inbox.items);
+        console.log('outbox: ', this.outbox.items);
+    }
 }
 
 export default QueueFromStacks;
---
+++ week-6/exercises/recursion.js
@@ -0,0 +1,51 @@
+// factorial
+// 5! is 5*4*3*2*1
+// can also be written as 5 * 4!
+
+export const factorial = (n) => {
+    if (!Number.isInteger(n) || n < 0) {
+        throw "Invalid input"
+    }
+    // console.log("n is now " + n)
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+// fibonacci
+// given n, return the nth item in the sequence
+// 1-1-2-3-5-8-13-21
+
+export const fibonacci = (n) => {
+    if (!Number.isInteger(n) || n <= 0) {
+        throw "Invalid input"
+    }
+    if (n == 1 || n == 2) {
+
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+// fibonacci
+// given n, return the nth item in the sequence
+// 1-1-2-3-5-8-13-21
+
+export const gcd = (m,n) => {
+    if (!Number.isInteger(m) || m <= 0) {
+        throw "Invalid input"
+    }
+    if (!Number.isInteger(n) || n <= 0) {
+        throw "Invalid input"
+    }
+    let small = Math.min(m,n);
+    let big = Math.max(m,n);
+    if (small == 0) {
+        return big;
+    } else {
+        return gcd(small, big%small)
+    }
+}
