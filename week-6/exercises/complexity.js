513a7e1783360a3b707c58d93628dfe6d62a5b58
Tushar Raj <traj@monsterindia.com>
2022-07-16 08:42:05 +0530

    space complexity

---
+++ week-6/exercises/complexity.js
@@ -0,0 +1,22 @@
+// space complexity
+// given an array of integers, return an array with all entries doubled
+
+//naive
+const double = (input) => {
+    let n = input.length;
+    let output = [];
+    for(let i = 0; i<n; i++) {
+        output[i] = input[i] * 2;
+    }
+
+    return output;
+}
+
+//better space
+const doubleInPlace = (input) => {
+    let n = input.length;
+    for(let i = 0; i<n; i++) {
+        input[i] = input[i] * 2;
+    }
+    return input;
+}
