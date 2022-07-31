5aea4e8d28e5dd7909cc0d606f2660e2f6fd18d6
Tushar Raj <traj@monsterindia.com>
2022-07-31 18:26:57 +0530

    insert and find

--- week-8/concepts/binary-search-tree.js
+++ week-8/concepts/binary-search-tree.js
@@ -15,6 +15,9 @@ class BinarySearchTree {
         return this.root === null;
     }
 
+
+    //iterative version of insert
+    /*
     insert(value) {
         let newNode = new Node(value);
         if (this.isEmpty) {
@@ -41,14 +44,72 @@ class BinarySearchTree {
             }
 
         }
+    }*/
+
+    insert(value) {
+        if (!Number.isInteger(value)) throw 'Invalid value';
+        let newNode = new Node(value);
+        if (this.isEmpty) {
+            this.root = newNode;
+            return;
+        } else {
+            this.#insertAt(newNode, this.root);
+        }
+    }
+
+    #insertAt(newNode, currentNode) {
+        if (newNode.value < currentNode.value) {
+            if (!currentNode.left) {
+                currentNode.left = newNode;
+                return;
+            } else {
+                this.#insertAt(newNode, currentNode.left);
+            }
+        } else if (newNode.value > currentNode.value) {
+            if (!currentNode.right) {
+                currentNode.right = newNode;
+                return;
+            } else {
+                this.#insertAt(newNode, currentNode.right);
+            }
+        } else {
+            throw 'Node with this value already exists';
+        }
+    }
+
+    find(value) {
+        if (this.isEmpty) {
+            return false;
+        }
+
+        let current = this.root;
+        let found = false;
+
+        while (current && !found) {
+            if (value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        return found;
     }
 }
 
 
-let bst = new BinarySearchTree();
-//   bst.insert(20)
-//   bst.insert(15)
-//   bst.insert(12)
-//   bst.insert(17)
-//   bst.insert(25)
-console.log(bst)
+try {
+    let bst = new BinarySearchTree();
+    bst.insert(20);
+    bst.insert(15);
+    bst.insert(12);
+    bst.insert(17);
+    bst.insert(25);
+    bst.insert(22);
+    bst.insert(27);
+    bst.insert(27);
+    console.log(bst);
+} catch (e) {
+    console.warn(e);
+}
